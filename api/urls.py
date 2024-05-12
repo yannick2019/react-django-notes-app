1e7f0bdf5edc4240ae6b15b21c3db5843fa6cada
Yannick Yanga <52249136+yannick2019@users.noreply.github.com>
2024-05-12 23:31:41 +0200

    set up django rest framework

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 
 urlpatterns = [
-    path("", views.getRoutes, name="routes")
+    path("", views.getRoutes, name="routes"),
+    path('api-auth/', include('rest_framework.urls'))
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
+@api_view(["GET"])
 def getRoutes(request):
     
     routes = [
@@ -36,4 +38,4 @@ def getRoutes(request):
             "description": "Delete and existing note"
         }
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
